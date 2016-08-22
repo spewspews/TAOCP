6f81fa70bca7bbf082253c5f2e6cef5b02cb4b7d
Benjamin Purcell <bpurcell@backstopsolutions.com>
2016-08-22 17:24:58 -0500

    Add infrastructure for constructing polynomials

--- x86_64/unix/2.2.4progamain.c
+++ x86_64/unix/2.2.4progamain.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 typedef struct Poly Poly;
 
@@ -12,6 +13,50 @@ struct Poly {
 int	add(Poly *q, Poly *p);
 
 Poly *avail;
+
+Poly*
+makepoly(void)
+{
+	Poly *p;
+
+	if(avail == NULL)
+		return NULL;
+	p = avail;
+	avail = avail->link;
+	p->abc = -1;
+	p->coef = 0;
+	p->link = p;
+	return p;
+}
+
+int
+addterm(Poly *p, int32_t coef, unsigned char a, unsigned char b, unsigned char c)
+{
+	Poly *q, *r;
+	int32_t abc;
+
+	abc = (a << 16) + (b << 8) + c;
+
+	for(r = p->link; abc < r->abc; r = p->link)
+		p = r;
+
+	if(abc == r->abc) {
+		r->coef += coef;
+		return 0;
+	}
+
+	if(avail == NULL)
+		return -1;
+	q = avail;
+	avail = avail->link;
+
+	q->coef = coef;
+	q->abc = abc;
+	q->link = r;
+
+	p->link = q;
+	return 0;
+}
 
 int
 main(int argc, char **argv)
