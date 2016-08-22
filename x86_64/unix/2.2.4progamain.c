bc456a88b541213b8f9f9437d076f3c176ccb453
Benjamin Scher Purcell <benjapurcell@gmail.com>
2016-08-22 09:07:23 -0500

    finish program a

---
+++ x86_64/unix/2.2.4progamain.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Poly Poly;
+
+struct Poly {
+	int32_t coef;
+	int32_t abc;
+	Poly *link;
+};
+
+int	add(Poly *q, Poly *p);
+
+Poly *avail;
+
+int
+main(int argc, char **argv)
+{
+	Poly *pool, *p;
+
+	pool = calloc(500, sizeof(*pool));
+	for(p = pool; p < pool+499; p++)
+		p->link = p+1;
+	p->link = NULL;
+	avail = pool;
+
+	add(p, avail);
+	exit(0);
+}
