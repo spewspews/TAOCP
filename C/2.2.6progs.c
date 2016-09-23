dfabd68bd53243d7cb87a4f6dee150205c246c14
Benjamin Scher Purcell <benjapurcell@gmail.com>
2016-09-23 16:29:56 -0500

    start adding C versions of matrix pivot

---
+++ C/2.2.6progs.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Matrix Matrix;
+typedef struct Node Node;
+
+struct Node
+{
+	Node *right;
+	Node *down;
+	unsigned int row;
+	unsigned int col;
+	double val;
+};
+
+struct Matrix
+{
+	Node *baserow;
+	Node *basecol;
+	unsigned int rows;
+	unsigned int cols;
+};
+
+int
+pivot(Matrix *m, unsigned int i0, unsigned int j0, double epsilon)
+{
+	Node **ptr, *p0, *q0, *p, *p1, *x;
+	unsigned int i, j;
+	double alpha;
+
+	if(i0 >= m->rows || j0 >= m->cols)
+		return -1;
+
+	ptr = calloc(sizeof(*ptr), m->cols);
+
+	for(p0 = m->baserow[i0].right; p0->col != j0; p0 = p0->right) {
+		if(p0->col == (unsigned int)-1 || p0->col > j0)
+			return -1;
+	}
+
+	// S1.
+	alpha = 1.0 / p0->val;
+	p0->val = 1.0;
+
+	// S2. process pivot row.
+	for(p0 = m->baserow[i0].right; p0->col != (unsigned int)-1; p0 = p0->right) {
+		ptr[p0->col] = m->basecol + p0->col;
+		p0->val *= alpha;
+	}
+
+	for(q0 = m->basecol[j0].down; q0->row != (unsigned int)-1; q0 = q0->down) {
+		if(q0->row == i0)
+			continue;
+		i = q0->row;
+		p = m->baserow + i;
+		p1 = p->right;
+		for(p0 = m->baserow[i0].right; p0->col != (unsigned int)-1; p0 = p0->right) {
+			if(p0->col == j0)
+				continue;
+			j = p0->col;
+			while(p1->col < j) {
+				p = p1;
+				p1 = p->right;
+			}
+			if(p1->col > j) {
+				while(ptr[j]->down->row < i)
+					ptr[j] = ptr[j]->down;
+				x = malloc(sizeof(*x));
+				x->row = i;
+				x->col = j;
+				x->right = p1;
+				x->down = ptr[j]->down;
+				ptr[j]->down = x;
+				p->right = x;
+			}
+			p1->val -= p0->val * q0->val;
+			if(fabs(p1->val) >= epsilon) {
+				ptr[j] = p1;
+				p = p1;
+			} else {
+				while(ptr[j]->down != p1)
+					ptr[j] = ptr[j]->down;
+				ptr[j]->down = p1->down;
+				p->right = p1->right;
+				free(p1);
+			}
+			p1 = p->right;
+		}
+		q0->val *= -alpha;
+	}
+	return 0;
+}
---
+++ C/2.2.6progsmain.c
@@ -0,0 +1,176 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <fmt.h>
+
+typedef struct Matrix Matrix;
+typedef struct Node Node;
+
+struct Node
+{
+	Node *right;
+	Node *down;
+	unsigned int row;
+	unsigned int col;
+	double val;
+};
+
+struct Matrix
+{
+	Node *baserow;
+	Node *basecol;
+	unsigned int rows;
+	unsigned int cols;
+};
+
+Matrix*
+makematrix(unsigned int rows, unsigned int cols)
+{
+	Matrix *m;
+	Node *n;
+
+	m = malloc(sizeof(*m));
+	if(m == NULL)
+		return NULL;
+
+	m->baserow = calloc(rows, sizeof(*m->baserow));
+	m->basecol = calloc(cols, sizeof(*m->basecol));
+	if(m->baserow == NULL || m->basecol == NULL) {
+		free(m);
+		return NULL;
+	}
+
+	for(n = m->baserow; n < m->baserow + rows; n++) {
+		n->right = n;
+		n->col = -1;
+	}
+
+	for(n = m->basecol; n < m->basecol + cols; n++) {
+		n->down = n;
+		n->row = -1;
+	}
+
+	m->rows = rows;
+	m->cols = cols;
+
+	return m;
+}
+
+int
+insert(Matrix *m, double val, unsigned int row, unsigned int col)
+{
+	Node *n, *p;
+
+	if(row >= m->rows || col >= m->cols)
+		return 0;
+
+	for(p = m->baserow+row; ; p = p->right) {
+		if(col > p->right->col)
+			continue;
+
+		if(col < p->right->col) {
+			n = malloc(sizeof(*n));
+			if(n == NULL)
+				return -1;
+		
+			n->row = row;
+			n->col = col;
+			n->val = val;
+
+			n->right = p->right;
+			p->right = n;
+			break;
+		}
+
+		if(col == p->right->col) {
+			n = NULL;
+			if(val == 0.0) {
+				p->right = p->right->right;
+				break;
+			}
+			p = p->right;
+			p->val = val;
+			break;
+		}
+	}
+
+	for(p = m->basecol+col; ; p = p->down) {
+		if(row > p->down->row)
+			continue;
+
+		if(row < p->down->row) {
+			n->down = p->down;
+			p->down = n;
+			break;
+		}
+
+		if(row == p->down->row) {
+			assert(n != NULL);
+			if(val == 0.0) {
+				n = p->down;
+				p->down = n->down;
+				free(n);
+				break;
+			}
+			p = p->down;
+			p->val = val;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int
+matrixfmt(Fmt *fmt)
+{
+	Matrix *m;
+	Node *n;
+	unsigned int i, j;
+	int r;
+	char c;
+
+	m = va_arg(fmt->args, Matrix*);
+
+	for(i = 0; i < m->rows; i++) {
+		n = m->baserow[i].right;
+		for(j = 0; j < m->cols; j++) {
+			c = j+1 < m->cols ? '\t' : '\n';
+			if(j == n->col) {
+				r = fmtprint(fmt, "%g%c", n->val, c);
+				n = n->right;
+			}
+			else
+				r = fmtprint(fmt, "N%c", c);
+
+			if(r == -1)
+				return -1;
+		}
+	}
+
+	return 0;
+}
+
+int pivot(Matrix*, unsigned int, unsigned int, double);
+
+int
+main(void)
+{
+	Matrix *m;
+
+	fmtinstall('M', matrixfmt);
+
+	m = makematrix(4, 4);
+	insert(m, 50, 0, 0);
+	insert(m, 10, 1, 0);
+	insert(m, 20, 1, 2);
+	insert(m, -30, 3, 0);
+	insert(m, -60, 3, 2);
+	insert(m, 5, 3, 3);
+	print("%M", m);
+
+	print("pivot success: %d\n", pivot(m, 1, 0, 1e-10));
+
+	print("%M", m);
+
+	exit(0);
+}
