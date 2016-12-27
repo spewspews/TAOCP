3b8605f5949c63f035d4c591f2a3e61e5acff7bd
Benjamin Scher Purcell <benjapurcell@gmail.com>
2016-12-26 19:36:18 -0600

    add x86_64 of inorder traversal

---
+++ x86_64/unix/2.3.1progtmain.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define	nelem(x)	(sizeof(x)/sizeof((x)[0]))
+
+typedef struct Node Node;
+
+struct Node
+{
+	Node *left, *right;
+	int val;
+};
+
+void inorder(Node*, void(*)(Node*));
+
+Node*
+insert(Node *r, Node *n)
+{
+	if(r == NULL)
+		return n;
+
+	if(n->val > r->val)
+		r->right = insert(r->right, n);
+	else if (n->val < r->val)
+		r->left = insert(r->left, n);
+	return r;
+}
+
+void
+printnode(Node *r)
+{
+	printf("Value is %d.\n", r->val);
+}
+
+Node pool[100];
+
+void
+debug(int d)
+{
+	printf("here at %d\n", d);
+}
+
+int
+main(void)
+{
+	Node *a, *r;
+
+	srand(time(NULL));
+	r = NULL;
+	for(a = pool; a < pool + nelem(pool); a++) {
+		a->val = (double)rand() / RAND_MAX * 1000;
+		printf("Inserting %d.\n", a->val);
+		r = insert(r, a);
+	}
+	printf("\nSorted:\n");
+	inorder(r, printnode);
+	exit(0);
+}
