d1632cbf5713e53b393f876e5c5e062c5d2c2778
Benjamin Scher Purcell <benjapurcell@gmail.com>
2017-01-08 10:52:33 -0600

    add mkfile, remove generated pool file, move the pool off of 0 (not sure if a bug or not with vm)

--- mix/genrands.c
+++ mix/genrands.c
@@ -2,7 +2,7 @@
 #include <libc.h>
 
 enum {
-	MAX = 1<<(6*3) - 1
+	MAX = (1 << 6*3) - 1
 };
 
 void
@@ -15,10 +15,15 @@ main(int argc, char **argv)
 	else
 		n = strtol(argv[1], nil, 10);
 
+	print("POOL\tEQU\t1\nORIG\tPOOL\n");
+
 	srand(time(nil));
-	for(i = 0; i < n; i++) {
+	for(i = 0; i < n-1; i++) {
 		print("\tCON\t%ld(3:5)\n", lnrand(MAX));
 		print("\tCON\t0(3:5)\n");
 	}
+	print("ENDP\tCON\t%ld(3:5)\n", lnrand(MAX));
+	print("\tCON\t0(3:5)\n");
+
 	exits(nil);
 }
